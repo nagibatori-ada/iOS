e5e01421fd0b7ea36481f7edeffa490a4115bcea
TemaSysoevMBP <artyom.sysoev@gmail.com>
2022-09-23 21:59:30 +0300

    project init

---
+++ Hack0922/Hack0922App.swift
@@ -0,0 +1,17 @@
+//
+//  Hack0922App.swift
+//  Hack0922
+//
+//  Created by Tema Sysoev on 23.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Hack0922App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
